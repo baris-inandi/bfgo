f6e29f137b53df06fe698f43274b4813c471884d
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-01-27 19:20:39 +0300

    compiler

---
+++ Makefile
@@ -0,0 +1,13 @@
+build:
+	rm -rf ./bin
+	mkdir ./bin
+	echo "building binary for linux amd64"
+	GOOS=linux GOARCH=amd64 go build -o ./bin/brainfuck-linux-amd64 ./compiler/brainfuck.go
+	echo "building binary for linux arm"
+	GOOS=linux GOARCH=arm go build -o ./bin/brainfuck-linux-arm ./compiler/brainfuck.go
+
+clean:
+	rm -rf ./bin
+
+install:
+	sudo go build -o /usr/bin/brainfuck ./compiler/brainfuck.go
---
+++ compiler/brainfuck.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+)
+
+var intermediate = ""
+var initByteBoilerplateRequired = false
+var fmtBoilerplateRequired = false
+var byteInBoilerplateRequired = false
+var byteOutBoilerplateRequired = false
+
+func readBrainfuck(f string) string {
+	fileBytes, err := ioutil.ReadFile(f)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return toValidBf(string(fileBytes))
+}
+
+func runeInSlice(a rune, list []rune) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func toValidBf(s string) string {
+	return strings.Map(
+		func(r rune) rune {
+			if runeInSlice(r, []rune{'<', '>', '+', '-', '.', '[', ']', ','}) {
+				return r
+			}
+			return -1
+		}, s,
+	)
+}
+
+func addCode(x string, init bool) {
+	if init {
+		initByteBoilerplateRequired = true
+		addCode("initByte(ptr);", false)
+	}
+	intermediate += x
+}
+
+func evalExpr(code string, ptr uint) string {
+	code = "/" + code + "/"
+	needsInit := true
+	for _, char := range code {
+		char := string(char)
+		switch char {
+		case ".":
+			addCode("bo();", true)
+			fmtBoilerplateRequired = true
+			byteOutBoilerplateRequired = true
+		case ",":
+			addCode("bi();", false)
+			fmtBoilerplateRequired = true
+			byteInBoilerplateRequired = true
+		case "[":
+			addCode("for {", false)
+		case "]":
+			addCode("if tape[ptr]==byte(0) {break}};", true)
+		case "<":
+			needsInit = true
+			addCode("ptr--;", false)
+		case ">":
+			needsInit = true
+			addCode("ptr++;", false)
+		case "+":
+			addCode("tape[ptr]++;", needsInit)
+			needsInit = false
+		case "-":
+			addCode("tape[ptr]--;", needsInit)
+			needsInit = false
+		}
+	}
+	return intermediate
+}
+
+func generateIntermediateCode(code string, outFile string) {
+	boilerplate := "package main;"
+	if fmtBoilerplateRequired {
+		boilerplate += "import \"fmt\";"
+	}
+	if initByteBoilerplateRequired {
+		boilerplate += "var tape = map[uint64]byte{}; var ptr = uint64(0);"
+		boilerplate += "func initByte(x uint64)byte{if val,ok:=tape[x];ok {return val};return byte(0)};"
+	}
+	if byteInBoilerplateRequired {
+		boilerplate += "func bi(){var bfIn byte;fmt.Printf(\"> \");fmt.Scanln(&bfIn);tape[ptr]=bfIn;};"
+	}
+	if byteOutBoilerplateRequired {
+		boilerplate += "func bo(){fmt.Printf(string(tape[ptr]))};"
+	}
+	goOut := boilerplate + "func main(){" + code + "}"
+
+	// generate temp .go file
+	f, _ := os.CreateTemp("", "brainfuck-*.go")
+	ioutil.WriteFile(f.Name(), []byte(goOut), 0640)
+	tempDir := (path.Dir(f.Name()))
+
+	// compile
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	arg := []string{"build", "-o", outFile, f.Name()}
+	cmd := exec.Command("go", arg...)
+	cmd.Stderr = stderr
+	cmd.Stdout = stdout
+	cmd.Dir = tempDir
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Brainfuck Compilation Error:")
+		fmt.Println("Error: ", stderr.String())
+	}
+
+	// cleanup
+	os.Remove(f.Name())
+}
+
+func generateOutFile(fileIn string, specifiedName string) string {
+	path, _ := os.Getwd()
+	path += "/"
+	outNoWd := ""
+	if specifiedName == "" {
+		fileInNameSplit := strings.Split(fileIn, "/")
+		fileInName := fileInNameSplit[len(fileInNameSplit)-1]
+		fileInNameDotSplit := strings.Split(fileInName, ".")
+		outNoWd = fileInNameDotSplit[0]
+	} else {
+		outNoWd = specifiedName
+	}
+	return path + outNoWd
+}
+
+func main() {
+	args := os.Args[1:]
+	outFile := ""
+	if len(args) < 1 {
+		fmt.Println("Brainfuck Compiler")
+		fmt.Println("Usage: ./brainfuck <file> <outputFile (optional)>")
+		os.Exit(0)
+	} else if len(args) >= 2 {
+		outFile = args[1]
+	}
+	fileIn := readBrainfuck(args[0])
+	out := evalExpr(fileIn, 0)
+	generateIntermediateCode(out, generateOutFile(args[0], outFile))
+}

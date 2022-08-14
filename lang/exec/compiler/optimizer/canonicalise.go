cfdac3ee36dce740d65abba50388770c2d16a896
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-08-14 18:47:22 +0300

    commit

--- lang/exec/compiler/optimizer/canonicalise.go
+++ lang/exec/compiler/optimizer/canonicalise.go
@@ -14,6 +14,7 @@ const IR_LITERAL_END = '/'
 func canonicalise(c lang.Code) lang.Code {
 
 	code := c.Inner
+	bindingDebugString := ""
 
 	changeShiftBf := func(loop string, amount int) string {
 		// [->+<] -> [->>>+<<<] ; where amount is 3
@@ -21,7 +22,15 @@ func canonicalise(c lang.Code) lang.Code {
 			strings.ReplaceAll(loop, ">", strings.Repeat(">", amount)),
 			"<", strings.Repeat("<", amount))
 	}
+	changeShiftConstBf := func(loop string, shift int, constant int) string {
+		return strings.ReplaceAll(
+			strings.ReplaceAll(
+				fmt.Sprintf(loop, strings.Repeat("+", constant)),
+				">", strings.Repeat(">", shift)),
+			"<", strings.Repeat("<", shift))
+	}
 	bindPatternToIR := func(code string, pattern string, ir string) string {
+		bindingDebugString += pattern + "  " + ir + "\n"
 		return strings.ReplaceAll(code, pattern, string(IR_LITERAL_START)+ir+string(IR_LITERAL_END))
 	}
 
@@ -33,11 +42,21 @@ func canonicalise(c lang.Code) lang.Code {
 	const BF__SUB_RIGHT_ALT = "[>-<-]"
 	const BF__SUB_LEFT = "[-<->]"
 	const BF__SUB_LEFT_ALT = "[<->-]"
+	// %s -> mult
+	const BF__MUL_LEFT = "[-<%s>]"
+	const BF__MUL_LEFT_ALT = "[<%s>-]"
+	const BF__MUL_RIGHT = "[->%s<]"
+	const BF__MUL_RIGHT_ALT = "[>%s<-]"
 
+	// %d -> shift
 	const IR__ADD_RIGHT = "*(p+%d)+=*p;*p=0;"
 	const IR__ADD_LEFT = "*(p-%d)+=*p;*p=0;"
 	const IR__SUB_RIGHT = "*(p+%d)-=*p;*p=0;"
 	const IR__SUB_LEFT = "*(p-%d)-=*p;*p=0;"
+	// %d -> shift
+	// %d -> constant multiplier
+	const IR__MUL_RIGHT = "*(p+%d)+=(*p)*%d;*p=0;"
+	const IR__MUL_LEFT = "*(p-%d)+=(*p)*%d;*p=0;"
 
 	// constant patterns, no shift
 	code = bindPatternToIR(code, "[-]", "*p=0;")
@@ -48,6 +67,15 @@ func canonicalise(c lang.Code) lang.Code {
 	c.VerboseOut("canonicalise.go: starting arithmetic canonicalisation with runs parameter ", runs)
 
 	for i := 1; i < runs; i++ {
+
+		for j := 1; j < runs; j++ {
+			// i -> shift, j -> constant
+			code = bindPatternToIR(code, changeShiftConstBf(BF__MUL_RIGHT, i, j), fmt.Sprintf(IR__MUL_RIGHT, i, j))
+			code = bindPatternToIR(code, changeShiftConstBf(BF__MUL_RIGHT_ALT, i, j), fmt.Sprintf(IR__MUL_RIGHT, i, j))
+
+			code = bindPatternToIR(code, changeShiftConstBf(BF__MUL_LEFT, i, j), fmt.Sprintf(IR__MUL_LEFT, i, j))
+			code = bindPatternToIR(code, changeShiftConstBf(BF__MUL_LEFT_ALT, i, j), fmt.Sprintf(IR__MUL_LEFT, i, j))
+		}
 
 		// patterns that add right
 		code = bindPatternToIR(code, changeShiftBf(BF__ADD_RIGHT, i), fmt.Sprintf(IR__ADD_RIGHT, i))
@@ -67,5 +95,6 @@ func canonicalise(c lang.Code) lang.Code {
 
 	}
 	c.Inner = code
+	c.AddDebugFile("ARITHMETIC_BINDINGS", bindingDebugString)
 	return c
 }

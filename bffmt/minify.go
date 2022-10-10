7c37afc6fdcc12c0b194820d91792802c7dd19ea
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-10 19:09:46 -0400

    Update minify.go

--- bffmt/minify.go
+++ bffmt/minify.go
@@ -2,12 +2,21 @@ package bffmt
 
 import (
 	"os"
+	"strings"
 
 	"github.com/baris-inandi/brainfuck/lang/readcode"
 )
 
 func MinifyFile(files ...string) {
 	minify := func(s string) string {
+		// if it never outputs, it's useless (except for memory-dump debugging)
+		if !strings.Contains(s, ".") {
+			return ""
+		}
+		// exploit mod 256 wrap-around
+		s = strings.ReplaceAll(s, strings.Repeat("+", 0x100), "")
+		s = strings.ReplaceAll(s, strings.Repeat("-", 0x100), "")
+
 		return s
 	}
 

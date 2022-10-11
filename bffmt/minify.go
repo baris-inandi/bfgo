cbc9524a98fde2d8b955309aec7fabc89bd3468a
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-10 20:57:51 -0400

    remove validator (for now)

--- bffmt/minify.go
+++ bffmt/minify.go
@@ -1,7 +1,6 @@
 package bffmt
 
 import (
-	"errors"
 	"os"
 	"strings"
 
@@ -9,30 +8,17 @@ import (
 )
 
 func MinifyFile(files ...string) {
-
-	/*
-		Check that all the square brackets are matched correctly.
-		it would be more helpful if it returned line/column,
-		but we don't need that, for now
-	*/
-	isValidSyntax := func(s string) bool {
-		return true
-	}
-
-	minify := func(s string) (string, error) {
-		if !isValidSyntax(s) {
-			return s, errors.New("SyntaxError: Mismatched brackets")
-		}
+	minify := func(s string) string {
 
 		// exploit mod 256 wrap-around
 		s = strings.ReplaceAll(s, strings.Repeat("+", 0x100), "")
 		s = strings.ReplaceAll(s, strings.Repeat("-", 0x100), "")
 
-		return s, nil
+		return s
 	}
 
 	for _, f := range files {
-		minified, _ := minify(readcode.ReadBrainfuck(f))
+		minified := minify(readcode.ReadBrainfuck(f))
 		err := os.WriteFile(f, []byte(minified), 0644)
 		if err != nil {
 			panic(err)

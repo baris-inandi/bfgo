d43d95ab83b47216ac80c2b1824fec543171930e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-12 22:35:14 -0400

    Update minify.go

--- bffmt/minify.go
+++ bffmt/minify.go
@@ -8,18 +8,36 @@ import (
 )
 
 func MinifyFile(files ...string) {
-	minify := func(s string) string {
 
+	/*
+		finds the address of the 1st `rune` that isn't in the set {"[", "]"}.
+		`start` ignores all runes before that index.
+	*/
+	indexOfNoBrace := func(s string, start int) int {
+		for start < len(s) {
+			if s[start] != "["[0] && s[start] != "]"[0] {
+				return start
+			}
+			start += 1
+		}
+		return -1
+	}
+
+	minify := func(s string) string {
 		// exploit mod 256 wrap-around
 		s = strings.ReplaceAll(s, strings.Repeat("+", 0x100), "")
 		s = strings.ReplaceAll(s, strings.Repeat("-", 0x100), "")
+
+		for i := indexOfNoBrace(s, 0); i < len(s); i += 1 {
+			//todo
+		}
 
 		return s
 	}
 
 	for _, f := range files {
 		minified := minify(readcode.ReadBrainfuck(f))
-		err := os.WriteFile(f, []byte(minified), 0644)
+		err := os.WriteFile(f, []byte(minified), 0o644)
 		if err != nil {
 			panic(err)
 		}

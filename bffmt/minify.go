843a102bfb1504df271a494020ccd8290dcbaa42
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-12 23:12:48 -0400

    Update minify.go

--- bffmt/minify.go
+++ bffmt/minify.go
@@ -10,11 +10,19 @@ import (
 func MinifyFile(files ...string) {
 
 	/*
-		finds the address of the 1st `rune` that isn't in the set {"[", "]"}.
+		Finds the address of the 1st `rune` that isn't in the set {"[", "]"}.
 		`start` ignores all runes before that index.
+		If `start` is negative, the index becomes relative to the end.
 	*/
 	indexOfNoBrace := func(s string, start int) int {
-		for start < len(s) {
+		size := len(s)
+		if start < 0 {
+			start += size
+		}
+		if start < 0 {
+			panic("Index out of bounds")
+		}
+		for start < size {
 			if s[start] != "["[0] && s[start] != "]"[0] {
 				return start
 			}
@@ -28,9 +36,36 @@ func MinifyFile(files ...string) {
 		s = strings.ReplaceAll(s, strings.Repeat("+", 0x100), "")
 		s = strings.ReplaceAll(s, strings.Repeat("-", 0x100), "")
 
-		// to-do: init a Map, to use negative indices
+		// simulated BF memory/tape
+		mem := map[int]uint8{}
+		// relative pointer
+		ptr := 0
+
 		for i := indexOfNoBrace(s, 0); i < len(s); i += 1 {
-			// to-do: do static simulation to reduce `+-><`
+			switch s[i] {
+			case "+"[0]:
+				{
+					mem[ptr] += 1
+					continue
+				}
+			case "-"[0]:
+				{
+					mem[ptr] -= 1
+					continue
+				}
+			case ">"[0]:
+				{
+					ptr += 1
+					continue
+				}
+			case "<"[0]:
+				{
+					ptr -= 1
+					continue
+				}
+			default:
+				i = indexOfNoBrace(s, i)
+			}
 		}
 
 		return s

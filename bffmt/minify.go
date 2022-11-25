c48c3ddcc3c0378cd22dc2cbaf691a369c7b78a9
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-25 15:05:09 -0400

    idiomatic Go

--- bffmt/minify.go
+++ bffmt/minify.go
@@ -11,21 +11,21 @@ import (
 func MinifyFile(files ...string) {
 
 	// returns a pair containing the char frequency of "+" and "-", respectively
-	var count_plus_minus = func(s string) (uint, uint) {
-		var plus_c uint = 0
-		var minus_c uint = 0
+	var counterPlusMinus = func(s string) (uint, uint) {
+		var plus uint = 0
+		var minus uint = 0
 
 		for i := 0; i < len(s); i++ {
 			c := s[i]
 			if c == "+"[0] {
-				plus_c += 1
+				plus += 1
 			}
 			if c == "-"[0] {
-				minus_c += 1
+				minus += 1
 			}
 		}
 
-		return plus_c, minus_c
+		return plus, minus
 	}
 
 	/*
@@ -53,32 +53,30 @@ func MinifyFile(files ...string) {
 		return -1
 	} */
 
-	/*
-		Optimizes the source, as explained in https://github.com/baris-inandi/brainfuck-go/issues/2 .
-		It assumes `s` only has valid opcodes.
-	*/
+	// Optimizes the source, as explained in https://github.com/baris-inandi/brainfuck-go/issues/2 .
+	// It assumes `s` only has valid opcodes.
 	var minify = func(s string) string {
 		// matches 256 consecutive + or - (exclusive, so mixes don't match)
-		var plus_minus_256 = regexp.MustCompile(`\+{256}|-{256}`)
-		s = plus_minus_256.ReplaceAllLiteralString(s, "")
+		var x256PlusMinus = regexp.MustCompile(`\+{256}|-{256}`)
+		s = x256PlusMinus.ReplaceAllLiteralString(s, "")
 
 		// matches pairs of BF opcodes that cancel each other (any order)
-		var mutual_cancel = regexp.MustCompile(`\+-|-\+|><|<>`)
+		var mutualCancel = regexp.MustCompile(`\+-|-\+|><|<>`)
 
 		var size int
 		// TODO: optimize later
 		for do := true; do; do = (size != len(s)) {
 			size = len(s)
-			s = mutual_cancel.ReplaceAllLiteralString(s, "")
+			s = mutualCancel.ReplaceAllLiteralString(s, "")
 		}
 
 		// matches + or - between square braces an even number of times (max 128)
-		var plus_minus_even = regexp.MustCompile(`\[(?:(?:\+\+){1,128}|(?:--){2,128})\]`)
-		s = plus_minus_even.ReplaceAllLiteralString(s, "[--]")
+		var evenPlusMinus = regexp.MustCompile(`\[(?:(?:\+\+){1,128}|(?:--){2,128})\]`)
+		s = evenPlusMinus.ReplaceAllLiteralString(s, "[--]")
 
 		// matches + or - between square braces an odd number of times (max 128)
-		var plus_minus_odd = regexp.MustCompile(`\[(?:(?:\+\+){0,128}\+|(?:--){1,128}-)\]`)
-		s = plus_minus_odd.ReplaceAllLiteralString(s, "[-]")
+		var oddPlusMinus = regexp.MustCompile(`\[(?:(?:\+\+){0,128}\+|(?:--){1,128}-)\]`)
+		s = oddPlusMinus.ReplaceAllLiteralString(s, "[-]")
 
 		/* // simulated BF memory/tape
 			var mem = map[int]uint8{}
@@ -117,13 +115,11 @@ func MinifyFile(files ...string) {
 				}
 			} */
 
-		/*
-			this is not the optimal way to do it,
-			because the counter still counts the cell-reseters themselves,
-			which adds an unwanted bias towards `-`
-		*/
-		plus_c, minus_c := count_plus_minus(s)
-		if plus_c >= minus_c {
+		// this is not the optimal way to do it,
+		// because the counter still counts the cell-reseters themselves,
+		// which adds an unwanted bias towards `-`
+		plusCount, minusCount := counterPlusMinus(s)
+		if plusCount >= minusCount {
 			s = strings.ReplaceAll(s, "[-]", "[+]")
 			s = strings.ReplaceAll(s, "[--]", "[++]")
 		}

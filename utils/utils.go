6c79e6c5232e17d70f77e53f3699fdf28c257b24
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-02-17 15:28:35 -0400

    fix RuneInSlice doc-comment?
    IDK why it was that way before

--- utils/utils.go
+++ utils/utils.go
@@ -1,13 +1,12 @@
 // other utility functions
 package utils
 
+/*
+Gets param a where a is a rune,
+and list is a list of runes,
+checks if a is in list
+*/
 func RuneInSlice(a rune, list []rune) bool {
-	/*
-		func runeInSlice
-			Gets param a where a is a rune,
-			and list is a list of runes,
-			checks if a is in list
-	*/
 	for _, b := range list {
 		if b == a {
 			return true

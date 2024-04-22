13d537b133942a29b7906863dc3702764e2fc2a7
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2024-04-22 18:44:15 +0300

    repl fixed

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/baris-inandi/bfgo
 go 1.17
 
 require (
-	github.com/urfave/cli/v2 v2.3.0
-	golang.org/x/text v0.3.7
+	github.com/urfave/cli/v2 v2.27.1
+	golang.org/x/text v0.14.0
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 )
--- lang/exec/interpreter/interpret.go
+++ lang/exec/interpreter/interpret.go
@@ -1,40 +1,10 @@
 package interpreter
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/baris-inandi/bfgo/lang"
-	"github.com/baris-inandi/bfgo/lang/readcode"
-	"github.com/baris-inandi/bfgo/utils"
 )
 
 func Interpret(code lang.Code) {
 	context := NewBfContext()
 	context.EvalExprWithContext(code.Inner)
-}
-
-func Repl() {
-	// welcome message
-	fmt.Println("bfgo REPL")
-	fmt.Println("Type 'exit' to exit.")
-	for {
-		// get prompt
-		fmt.Print("bfgo> ")
-		input := ""
-		fmt.Scanln(&input)
-		// handle exiting
-		if input == "exit" {
-			fmt.Println("\nGoodbye!")
-			os.Exit(0)
-		}
-		if input == "quit" {
-			fmt.Println("Type 'exit' to exit.")
-		}
-		EvalExpr(readcode.ToValidBF(input))
-		// print an empty line if a print statement is present in the input
-		if utils.RuneInSlice('.', []rune(input)) {
-			fmt.Println()
-		}
-	}
 }
